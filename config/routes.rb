9f1cb8b52f947eaa31522f94b92beafeafa7fc56
Krishna Bhat <krishna.bhat@gmail.com>
2014-09-01 19:52:58 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HosaApp::Application.routes.draw do
 
-  get "users/new"
+  # get "users/new" replaced with below to get full REST style URLs to work
+  resources :users
   root 'static_pages#home'
   match '/help', to:'static_pages#help', via: 'get'
   match '/about', to:'static_pages#about', via: 'get'
